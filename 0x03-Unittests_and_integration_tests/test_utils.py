46c9c50d0b97a506ed3c103832ccbc0ba6553fb0
Brandon Khumalo <127394576+Brandonkhumalo@users.noreply.github.com>
2025-06-01 18:25:55 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -142,6 +142,7 @@ class TestMemoize(unittest.TestCase):
 
     def test_memoize(self):
         """Test that memoize caches the result of the method call."""
+
         class TestClass:
             def a_method(self):
                 return 42
@@ -150,11 +151,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(
-            TestClass,
-            'a_method',
-            return_value=42
-        ) as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
             test = TestClass()
             result_1 = test.a_property
             result_2 = test.a_property
