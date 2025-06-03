ea420cd564a7da851bc07cb85d6fe8a1b48d3557
Brandon <Apex2.0predator@gmail.com>
2025-06-03 13:32:54 +0200

    fix middleware

--- Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
@@ -1,9 +1,9 @@
 import logging
 from datetime import datetime
 
-# Configure logger
+# Set up a logger
 logger = logging.getLogger("request_logger")
-handler = logging.FileHandler("user_requests.log")  # Log file
+handler = logging.FileHandler("user_requests.log")  # File where logs will be stored
 formatter = logging.Formatter('%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -11,12 +11,12 @@ logger.setLevel(logging.INFO)
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
-        self.get_response = get_response
+        self.get_response = get_response  # Required initialization step
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else "Anonymous"
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
         logger.info(log_message)
 
-        response = self.get_response(request)
+        response = self.get_response(request)  # Proceed to the next middleware/view
         return response
