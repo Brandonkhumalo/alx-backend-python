261e99eea70f7673f7150e00064555f7d0c64d70
Brandon <Apex2.0predator@gmail.com>
2025-06-03 13:38:34 +0200

    add new middleware

--- Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/chats/middleware.py
@@ -1,22 +1,36 @@
+from datetime import datetime, time
+from django.http import HttpResponseForbidden
 import logging
-from datetime import datetime
 
-# Set up a logger
-logger = logging.getLogger("request_logger")
-handler = logging.FileHandler("user_requests.log")  # File where logs will be stored
-formatter = logging.Formatter('%(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# Set up logging configuration
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('request_logs.log')
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
-        self.get_response = get_response  # Required initialization step
+        self.get_response = get_response
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else "Anonymous"
-        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        logger.info(log_message)
+        logger.info(f"User: {user} - Path: {request.path}")
+        return self.get_response(request)
 
-        response = self.get_response(request)  # Proceed to the next middleware/view
-        return response
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        now = datetime.now().time()
+        start_restrict = time(21, 0)  # 9 PM
+        end_restrict = time(6, 0)     # 6 AM
+
+        if now >= start_restrict or now < end_restrict:
+            return HttpResponseForbidden("Access to the messaging app is restricted during these hours (9PM to 6AM).")
+
+        return self.get_response(request)
