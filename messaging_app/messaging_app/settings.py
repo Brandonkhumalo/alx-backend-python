7864632f0223bc9edf2ebcaf9ca23cca25780faf
Brandon <Apex2.0predator@gmail.com>
2025-06-06 09:17:30 +0200

    added drf

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 
     'DEFAULT_PERMISSION_CLASSES': [
