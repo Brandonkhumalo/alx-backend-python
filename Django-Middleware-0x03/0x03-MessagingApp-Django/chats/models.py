1239e817ab0d20649632d99c7a4ef9870c487745
Brandon <Apex2.0predator@gmail.com>
2025-06-06 08:46:05 +0200

    fixed folder

---
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/chats/models.py
@@ -0,0 +1,45 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLE_CHOICES = (
+    ('admin', 'Admin'),
+    ('moderator', 'Moderator'),
+    ('user', 'User'),
+)
+
+# Custom User model named "User"
+class User(AbstractUser):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+# Conversation model
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+# Message model
+class Message(models.Model):
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.message_id} from {self.sender.email}"
