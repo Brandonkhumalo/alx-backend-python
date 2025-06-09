56075200d2f499dbb3ea19bff6010077b3853eb6
Brandon <Apex2.0predator@gmail.com>
2025-06-09 10:04:38 +0200

    added new signal

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import Message, Notification, MessageHistory
+
+User = get_user_model()
 
 @receiver(post_save, sender=Message)
 def create_notification_on_new_message(sender, instance, created, **kwargs):
@@ -21,3 +24,18 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True  # Mark message as edited
         except Message.DoesNotExist:
             pass  # New message, no edit to log
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages where the user is sender or receiver
+    user_messages = Message.objects.filter(sender=instance) | Message.objects.filter(receiver=instance)
+    user_message_ids = user_messages.values_list('id', flat=True)
+
+    # Delete associated message histories
+    MessageHistory.objects.filter(message_id__in=user_message_ids).delete()
+
+    # Delete the messages themselves
+    user_messages.delete()
+
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
